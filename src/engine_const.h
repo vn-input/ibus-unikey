4fca7c45e4d3cbd5d8a5b04e4b571ee55b80f61b
Mr.LeQuocTuan <Mr.LeQuocTuan@23244130-415d-11de-b466-9154fc769079>
2009-07-06 18:31:59 +0000

    commit miss file
    git-svn-id: https://ibus-unikey.googlecode.com/svn/branches/0.2@68 23244130-415d-11de-b466-9154fc769079

---
+++ src/engine_const.h
@@ -0,0 +1,16 @@
+#ifndef __ENGINE_CONST_H__
+#define __ENGINE_CONST_H__
+
+// define DEFAULT unikey options
+#define UNIKEY_OPT_FREEMARKING          TRUE
+#define UNIKEY_OPT_MODERNSTYLE          FALSE
+#define UNIKEY_OPT_MACROENABLED         FALSE
+#define UNIKEY_OPT_SPELLCHECKENABLED    TRUE
+#define UNIKEY_OPT_AUTONONVNRESTORE     TRUE
+
+#define UNIKEY_OPT_PROCESSWATBEGIN      TRUE
+
+#define UNIKEY_MACRO_FILE               ".ibus/unikey/macro"
+
+#endif
+
